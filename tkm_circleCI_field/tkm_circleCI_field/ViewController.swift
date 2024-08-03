25eb3814841114cce99921d855071c5573c992e2
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-08-03 11:36:00 +0900

    ローカルでエラーが発生した際にCIもコケるか確認

--- tkm_circleCI_field/tkm_circleCI_field/ViewController.swift
+++ tkm_circleCI_field/tkm_circleCI_field/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+error
 
 }
 
