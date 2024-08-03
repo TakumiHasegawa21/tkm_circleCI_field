03febca511bc3de4e57de38f27911546d86efb7f
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-08-03 11:07:21 +0900

    プロジェクトを追加

---
+++ tkm_circleCI_field/tkm_circleCI_field/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tkm_circleCI_field
+//
+//  Created by 長谷川 拓海(Takumi Hasegawa) on 2024/08/03.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
