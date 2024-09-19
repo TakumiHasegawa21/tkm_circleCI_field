b5e50197e613bbd50193913eb84ea7501b3e8fff
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2024-09-19 15:03:55 +0900

    rerun用

--- tkm_circleCI_field/ViewController.swift
+++ tkm_circleCI_field/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("テスト")
+        print("rerun")
         // Do any additional setup after loading the view.
     }
 
